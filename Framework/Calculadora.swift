c6e59113afaab83e16682b48700ee325838b7617
augusto <augustohenriquealmeidasilva@gmail.com>
2017-06-02 16:25:55 -0300

    Criado classe calculadora, funções básicas e função de potenciação

---
+++ Framework/Calculadora.swift
@@ -0,0 +1,48 @@
+//
+//  Calculadora.swift
+//  Framework
+//
+//  Created by Augusto Henrique de Almeida Silva on 02/06/17.
+//  Copyright © 2017 Eric Winston. All rights reserved.
+//
+
+import Foundation
+
+public class Calculadora {
+    
+    public func somar(numberA: Double, numberB: Double) -> Double{
+        return numberA + numberB
+    }
+    
+    public func subtrair(numberA: Double, numberB: Double) -> Double {
+        return numberA - numberB
+    }
+    
+    public func multiplicar(numberA: Double, numberB: Double) -> Double {
+        return numberA * numberB
+    }
+    
+    public func dividir(numberA: Double, numberB: Double) -> Double {
+        return numberA / numberB
+    }
+    
+    public func elevado(numberA: Double, numberB: Double) -> Double {
+        
+        var resultado: Double = 1
+        
+        if numberB == 0 {
+            return resultado
+        }
+        
+        for _ in 0...Int(numberB) {
+            resultado *= numberA
+        }
+        
+        return resultado
+    }
+    
+    public func percent(numberA: Double, numberB: Double) -> Double {
+        return numberA / numberB
+    }
+    
+}
