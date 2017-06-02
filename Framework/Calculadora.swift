ae50d2ea224563fa8045fb9a6ae053a683b936a4
Eric Winston <winstoncire@gmail.com>
2017-06-02 16:49:34 -0300

    adiciona os comentarios

--- Framework/Calculadora.swift
+++ Framework/Calculadora.swift
@@ -10,39 +10,79 @@ import Foundation
 
 public class Calculadora {
     
+    public init(){
+        
+    }
+    
+    /**
+     Soma dois numeros
+     - Parameters:
+        - numberA: Primeiro numero da soma
+        - numberB: Segundo numero da soma
+    */
     public func somar(numberA: Double, numberB: Double) -> Double{
         return numberA + numberB
     }
     
+    /**
+     Subtrai dois numeros
+     - Parameters:
+     - numberA: Primeiro numero da subtração
+     - numberB: Segundo numero da subtração
+     */
     public func subtrair(numberA: Double, numberB: Double) -> Double {
         return numberA - numberB
     }
     
+    /**
+     Multiplica dois numeros
+     - Parameters:
+     - numberA: Primeiro numero da multiplicação
+     - numberB: Segundo numero da multiplicação
+     */
     public func multiplicar(numberA: Double, numberB: Double) -> Double {
         return numberA * numberB
     }
     
+    /**
+     Divide dois numeros
+     - Parameters:
+     - numberA: Primeiro numero da divisão
+     - numberB: Segundo numero da divisão
+     */
     public func dividir(numberA: Double, numberB: Double) -> Double {
         return numberA / numberB
     }
     
-    public func elevado(numberA: Double, numberB: Double) -> Double {
+    /**
+     Eleva um numero ao outro numero informado
+     - Parameters:
+     - numberA: Numero que sera elevado
+     - elevatedNumber: Numero da elevação
+     */
+    public func elevado(numberA: Double, elevatedNumber: Double) -> Double {
         
         var resultado: Double = 1
         
-        if numberB == 0 {
+        if elevatedNumber == 0 {
             return resultado
         }
         
-        for _ in 0...Int(numberB) {
+        for _ in 0...Int(elevatedNumber) {
             resultado *= numberA
         }
         
         return resultado
     }
     
-    public func percent(numberA: Double, numberB: Double) -> Double {
-        return numberA / numberB
+    /**
+     Tira uma porcentagem de um numero
+     - Parameters:
+     - numberA: Numero
+     - percent: Porcentagem a ser tirado do numero
+     */
+    public func percent(numberA: Double, percent: Double) -> Double {
+        return numberA/100 * percent
     }
     
 }
